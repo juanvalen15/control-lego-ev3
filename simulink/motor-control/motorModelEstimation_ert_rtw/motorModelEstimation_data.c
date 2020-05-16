e15ef47c3e219f6051b88f14b1d30e8909dd9cad
juan <jdvalenciapayan@gmail.com>
2020-05-16 21:46:24 +0200

    added experiments with ev3RT

---
+++ simulink/motor-control/motorModelEstimation_ert_rtw/motorModelEstimation_data.c
@@ -0,0 +1,40 @@
+/*
+ * motorModelEstimation_data.c
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "motorModelEstimation".
+ *
+ * Model version              : 1.22
+ * Simulink Coder version : 9.1 (R2019a) 23-Nov-2018
+ * C source code generated on : Sat May 16 18:11:56 2020
+ *
+ * Target selection: ert.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: ARM Compatible->ARM 9
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "motorModelEstimation.h"
+#include "motorModelEstimation_private.h"
+
+/* Block parameters (default storage) */
+P_motorModelEstimation_T motorModelEstimation_P = {
+  /* Expression: 1
+   * Referenced by: '<Root>/Step'
+   */
+  1.0,
+
+  /* Expression: 0
+   * Referenced by: '<Root>/Step'
+   */
+  0.0,
+
+  /* Expression: 20
+   * Referenced by: '<Root>/Step'
+   */
+  20.0
+};
---
+++ simulink/motor-control/motorModelEstimation_ert_rtw/motorModelEstimation_targ_data_map.m
@@ -0,0 +1,207 @@
+  function targMap = targDataMap(),
+
+  ;%***********************
+  ;% Create Parameter Map *
+  ;%***********************
+      
+    nTotData      = 0; %add to this count as we go
+    nTotSects     = 1;
+    sectIdxOffset = 0;
+    
+    ;%
+    ;% Define dummy sections & preallocate arrays
+    ;%
+    dumSection.nData = -1;  
+    dumSection.data  = [];
+    
+    dumData.logicalSrcIdx = -1;
+    dumData.dtTransOffset = -1;
+    
+    ;%
+    ;% Init/prealloc paramMap
+    ;%
+    paramMap.nSections           = nTotSects;
+    paramMap.sectIdxOffset       = sectIdxOffset;
+      paramMap.sections(nTotSects) = dumSection; %prealloc
+    paramMap.nTotData            = -1;
+    
+    ;%
+    ;% Auto data (motorModelEstimation_P)
+    ;%
+      section.nData     = 3;
+      section.data(3)  = dumData; %prealloc
+      
+	  ;% motorModelEstimation_P.Step_Time
+	  section.data(1).logicalSrcIdx = 0;
+	  section.data(1).dtTransOffset = 0;
+	
+	  ;% motorModelEstimation_P.Step_Y0
+	  section.data(2).logicalSrcIdx = 1;
+	  section.data(2).dtTransOffset = 1;
+	
+	  ;% motorModelEstimation_P.Step_YFinal
+	  section.data(3).logicalSrcIdx = 2;
+	  section.data(3).dtTransOffset = 2;
+	
+      nTotData = nTotData + section.nData;
+      paramMap.sections(1) = section;
+      clear section
+      
+    
+      ;%
+      ;% Non-auto Data (parameter)
+      ;%
+    
+
+    ;%
+    ;% Add final counts to struct.
+    ;%
+    paramMap.nTotData = nTotData;
+    
+
+
+  ;%**************************
+  ;% Create Block Output Map *
+  ;%**************************
+      
+    nTotData      = 0; %add to this count as we go
+    nTotSects     = 2;
+    sectIdxOffset = 0;
+    
+    ;%
+    ;% Define dummy sections & preallocate arrays
+    ;%
+    dumSection.nData = -1;  
+    dumSection.data  = [];
+    
+    dumData.logicalSrcIdx = -1;
+    dumData.dtTransOffset = -1;
+    
+    ;%
+    ;% Init/prealloc sigMap
+    ;%
+    sigMap.nSections           = nTotSects;
+    sigMap.sectIdxOffset       = sectIdxOffset;
+      sigMap.sections(nTotSects) = dumSection; %prealloc
+    sigMap.nTotData            = -1;
+    
+    ;%
+    ;% Auto data (motorModelEstimation_B)
+    ;%
+      section.nData     = 1;
+      section.data(1)  = dumData; %prealloc
+      
+	  ;% motorModelEstimation_B.Step
+	  section.data(1).logicalSrcIdx = 0;
+	  section.data(1).dtTransOffset = 0;
+	
+      nTotData = nTotData + section.nData;
+      sigMap.sections(1) = section;
+      clear section
+      
+      section.nData     = 1;
+      section.data(1)  = dumData; %prealloc
+      
+	  ;% motorModelEstimation_B.sensorSignals
+	  section.data(1).logicalSrcIdx = 1;
+	  section.data(1).dtTransOffset = 0;
+	
+      nTotData = nTotData + section.nData;
+      sigMap.sections(2) = section;
+      clear section
+      
+    
+      ;%
+      ;% Non-auto Data (signal)
+      ;%
+    
+
+    ;%
+    ;% Add final counts to struct.
+    ;%
+    sigMap.nTotData = nTotData;
+    
+
+
+  ;%*******************
+  ;% Create DWork Map *
+  ;%*******************
+      
+    nTotData      = 0; %add to this count as we go
+    nTotSects     = 1;
+    sectIdxOffset = 2;
+    
+    ;%
+    ;% Define dummy sections & preallocate arrays
+    ;%
+    dumSection.nData = -1;  
+    dumSection.data  = [];
+    
+    dumData.logicalSrcIdx = -1;
+    dumData.dtTransOffset = -1;
+    
+    ;%
+    ;% Init/prealloc dworkMap
+    ;%
+    dworkMap.nSections           = nTotSects;
+    dworkMap.sectIdxOffset       = sectIdxOffset;
+      dworkMap.sections(nTotSects) = dumSection; %prealloc
+    dworkMap.nTotData            = -1;
+    
+    ;%
+    ;% Auto data (motorModelEstimation_DW)
+    ;%
+      section.nData     = 4;
+      section.data(4)  = dumData; %prealloc
+      
+	  ;% motorModelEstimation_DW.Scope_PWORK.LoggedData
+	  section.data(1).logicalSrcIdx = 0;
+	  section.data(1).dtTransOffset = 0;
+	
+	  ;% motorModelEstimation_DW.Scope1_PWORK.LoggedData
+	  section.data(2).logicalSrcIdx = 1;
+	  section.data(2).dtTransOffset = 1;
+	
+	  ;% motorModelEstimation_DW.ToWorkspace_PWORK.LoggedData
+	  section.data(3).logicalSrcIdx = 2;
+	  section.data(3).dtTransOffset = 2;
+	
+	  ;% motorModelEstimation_DW.ToWorkspace1_PWORK.LoggedData
+	  section.data(4).logicalSrcIdx = 3;
+	  section.data(4).dtTransOffset = 3;
+	
+      nTotData = nTotData + section.nData;
+      dworkMap.sections(1) = section;
+      clear section
+      
+    
+      ;%
+      ;% Non-auto Data (dwork)
+      ;%
+    
+
+    ;%
+    ;% Add final counts to struct.
+    ;%
+    dworkMap.nTotData = nTotData;
+    
+
+
+  ;%
+  ;% Add individual maps to base struct.
+  ;%
+
+  targMap.paramMap  = paramMap;    
+  targMap.signalMap = sigMap;
+  targMap.dworkMap  = dworkMap;
+  
+  ;%
+  ;% Add checksums to base struct.
+  ;%
+
+
+  targMap.checksum0 = 2391727121;
+  targMap.checksum1 = 574667379;
+  targMap.checksum2 = 945483668;
+  targMap.checksum3 = 1090510143;
+
